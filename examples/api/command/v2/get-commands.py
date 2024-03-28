26ed42ebeb766190c294c8edf2fa4056449cb562
normaljosh <josh@normal.dev>
2024-03-28 16:31:37 -0400

    add initial auth example

--- examples/api/command/v2/get-commands.py
+++ examples/api/command/v2/get-commands.py
@@ -4,7 +4,12 @@ import sys
 import json
 import requests
 
-nfurl = os.getenv("NFURL", "http://localhost:8080")                        
+sys.path.append("../..")
+from auth.v1.auth import get_token
 
-res = requests.get(nfurl + "/api/v2/command")
+nfurl = os.getenv("NFURL", "http://localhost:8080")
+headers = {"Authorization": f"Bearer {get_token()}"}
+
+res = requests.get(nfurl + "/api/v2/command", headers=headers)
+
 json.dump(res.json(), sys.stdout, indent=2)
