7e6ede16e80be7a12ea83823f92711af551fca5c
Stephen Dawson-Haggerty <sdhags@gmail.com>
2024-04-01 09:55:28 -0400

    Add basic auth support to examples

--- examples/api/helpers.py
+++ examples/api/helpers.py
@@ -6,14 +6,15 @@ import sys
 import json
 import os
 import requests
-
-base = os.getenv("NFURL", "http://localhost:8080")
-client_id = os.getenv("NF_CLIENT_ID")
-client_secret = os.getenv("NF_CLIENT_SECRET")
+import argparse
+import base64
 
 class JsonOauth(requests.auth.AuthBase):
-    def __init__(self):
+    def __init__(self, base, oauth=None, creds=None):
         self.token = None
+        self.base = base
+        self.oauth = oauth
+        self.creds = creds
 
     def handle_401(self, r, **kwargs):
         """
@@ -23,9 +24,9 @@ class JsonOauth(requests.auth.AuthBase):
         r.content
         r.close()
 
-        res = requests.post(base + "/api/v1/auth/token", json={
-            "client_id": client_id,
-            "client_secret": client_secret,
+        res = requests.post(self.base + "/api/v1/auth/token", json={
+            "client_id": self.oauth[0],
+            "client_secret": self.oauth[1],
             "grant_type": "client_credentials"
         })
 
@@ -34,7 +35,6 @@ class JsonOauth(requests.auth.AuthBase):
 
         info = res.json()
         self.token = info["accessToken"]
-        print (self.token)
 
         prep = r.request.copy()
         prep.headers["Authorization"] = "Bearer " + self.token
@@ -44,23 +44,41 @@ class JsonOauth(requests.auth.AuthBase):
         return _r
 
     def __call__(self, r):
-        if self.token is None:
-            r.register_hook("response", self.handle_401)
-        else:
-            r.headers["Authorization"] = "Bearer " + self.token
+        if self.oauth is not None:
+            # if using token auth, we need to request an access token
+            if self.token is None:
+                r.register_hook("response", self.handle_401)
+            else:
+                r.headers["Authorization"] = "Bearer " + self.token
+        elif self.creds is not None:
+            # if using basic, just send the credentials
+            r.headers["Authorization"] = "Basic " + base64.b64encode(self.creds.encode("utf-8")).decode("utf-8")
+
         return r
 
 class NfClient(object):
     """A simple wrapper for requests wihch adds authentication tokens"""
 
     def __init__(self):
-        self.auth = JsonOauth()
+        # pull the connection URL and so on from the environment
+        parser = argparse.ArgumentParser("nf-client")
+        parser.add_argument("--client-id", default=os.getenv("NF_CLIENT_ID"))
+        parser.add_argument("--client-secret", default=os.getenv("NF_CLIENT_SECRET"))
+        parser.add_argument("-u", "--user")
+        parser.add_argument("--url", default=os.getenv("NFURL", "http://localhost:8080"))
+        args = parser.parse_args()
+
+
+        self.base = args.url.rstrip("/")
+        self.auth = JsonOauth(self.base, oauth=(args.client_id, args.client_secret) if
+                              args.client_id and args.client_secret else None,
+                              creds=args.user)
     
     def get(self, path, *args, **kwargs):
-        return requests.get(base + path, auth=self.auth)
+        return requests.get(self.base + path, *args, auth=self.auth, **kwargs)
 
     def post(self, path, json={}, *args, **kwargs):
-        return requests.post(base + path, auth=self.auth, json=json)
+        return requests.post(self.base + path, auth=self.auth, json=json)
     
 
 def print_response(res):
