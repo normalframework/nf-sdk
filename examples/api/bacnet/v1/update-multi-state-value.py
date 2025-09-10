73cb27591d6739e2dbb5d22f3692dc631bc359a5
Stephen Dawson-Haggerty <sdhags@gmail.com>
2025-09-10 09:55:05 -0400

    Add examples of updating other properties

--- examples/api/bacnet/v1/update-multi-state-value.py
+++ examples/api/bacnet/v1/update-multi-state-value.py
@@ -11,17 +11,36 @@ from helpers import NfClient, print_response
 client = NfClient()
 
 res = client.patch("/api/v1/bacnet/local", {
+    #"localDeviceInstanceOffset": 30184,
+    # "uuid": "f17a5270-70a1-502b-bb6c-a16c2126c839",
     "object_id": {
-        "object_type": "OBJECT_MULTI_STATE_VALUE",
+        "object_type": "OBJECT_MULTI_STATE_INPUT",
         "instance": 1, # create a new object
-    },
+    },    
     "props": [
         {
             "property": "PROP_PRESENT_VALUE",
             "value": {
-                "enumerated": 1,
+                "enumerated": 3,
             },
         },
+        {
+            "property": "PROP_OUT_OF_SERVICE",
+            "value": {
+                "boolean": True,
+            }
+        },
+        {
+            "property": "PROP_STATUS_FLAGS",
+            "value": {
+                "bitString": {
+                    "length": 4,
+                    "setBits": [
+                        1, 2
+                    ]
+                }
+            }
+        }
     ]
 })
 print_response(res)
